99619afe67065617451e430a8015784343de5d19
NgocNguyenGit <46431731+NgocNguyenGit@users.noreply.github.com>
2021-01-04 04:04:00 +0200

    Add files via upload

---
+++ Weekly Task 1/t2.c
@@ -0,0 +1,14 @@
+//
+//  t2.c
+//  week_1
+//
+//  Created by Nguyen Le Khanh Ngoc on 03/09/2020.
+//  Copyright © 2020 Nguyen Le Khanh Ngoc. All rights reserved.
+//
+
+#include <stdio.h>
+int main(){
+    int i = 111;
+    puts(i);
+    return 0;
+}
---
+++ Weekly Task 1/test.c
@@ -0,0 +1,13 @@
+//
+//  test.c
+//  week_1
+//
+//  Created by Nguyen Le Khanh Ngoc on 03/09/2020.
+//  Copyright © 2020 Nguyen Le Khanh Ngoc. All rights reserved.
+//
+
+#include <stdio.h>
+int main(){
+    puts("test");
+    return 0;
+}
